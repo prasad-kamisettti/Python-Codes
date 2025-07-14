4ccd652122a4e3abc3b0e7f223e48f9d3cbd95eb
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-07-14 12:17:51 -0500

    added another version to find armstrong using functions

--- armstrong.py
+++ armstrong.py
@@ -1,9 +1,11 @@
+##without functions 
+  
 number = int(input("enter a number: ")) #taking input
 original_number = number #Storing the inout number into original number to compare later
 
-sum = 0  #Initilize sum to 0
+sum = 0  
 
-number_of_digits = len(str(abs(number))) # find the number of digits 
+number_of_digits = len(str(abs(number))) 
 
 while number > 0: 
     num = number%10 #extract the last digit
@@ -15,4 +17,25 @@ if sum==original_number:
     print(f"{original_number} is a armstrong number")
 else:
     print(f"{original_number} is not an armstrong number")
+    
+    
+    
+##using functions  
+
+def armstrong(number):
+    num_str = str(number)
+    num_digits = len(num_str)
+    sum = 0
+
+    for digit in num_str:
+        sum += int(digit) ** num_digits
+
+    return sum == number
+
+number = int(input("enter a number: ")) 
+if armstrong(number):
+    print(f"{number} is an Armstrong number.")
+else:
+    print(f"{number} is not an Armstrong number.")
+
     
