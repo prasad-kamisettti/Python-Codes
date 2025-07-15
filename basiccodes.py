f584388fafbab2ed6c86eb8dd870957579f883f7
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-07-15 09:52:57 -0500

    added the code to find sum of squares of first N natural numbers

---
+++ basiccodes.py
@@ -0,0 +1,19 @@
+radius = int(input("enter the radius of a circle: "))
+pi = 3.14
+# formulae to find area of circle
+area = pi*(radius*radius)
+
+print (f"Area of circle is {area}")
+
+############################## sum of squares of first N natural numbers #######################################################
+
+n = int(input("enter a number: "))
+sum=0 
+
+for i in range (n+1):
+    i = i**2
+    sum = sum+i
+    
+print(f"sum of squares of {n} natural numbers is {sum}")
+    
+    
--- circlearea.py
+++
@@ -1,7 +0,0 @@
-radius = int(input("enter the radius of a circle: "))
-pi = 3.14
-
-#formulae to find area of circle
-area = pi*(radius*radius)
-
-print (f"Area of circle is {area}")
