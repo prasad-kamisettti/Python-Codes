885b075ab72c21c74393404b90521ad180ef567d
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-07-17 12:16:01 -0500

    pushing the code to swap first and last element of list using temp and function

--- lists.py
+++ lists.py
@@ -1,5 +1,17 @@
 ############### program to interchange the first and last element of a list #################################################
 
-my_list = [1,2,3,4]
-my_list[0],my_list[-1] = my_list[-1],my_list[0]
-print(f"updated list after swapping first and last element is {my_list}")
+# my_list = [1,2,3,4]
+# my_list[0],my_list[-1] = my_list[-1],my_list[0]
+# print(f"updated list after swapping first and last element is {my_list}")
+
+#################################### using functions and temp variable #############################################################
+def swapList(my_list):
+    size = len(my_list)
+    
+    temp = my_list[0]
+    my_list[0] = my_list[size-1]
+    my_list[size-1] = temp
+    return my_list
+my_list = list(map(int, input("Enter a numbers seperated by space: ").split()))
+
+print(swapList(my_list))
