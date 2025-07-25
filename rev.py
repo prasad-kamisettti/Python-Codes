7015aecbeabf228ee23515b2eef9df34af06e332
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-07-25 08:33:20 -0500

    pushing the code to find reverse of a string

--- rev.py
+++ rev.py
@@ -1,15 +1,30 @@
 
 ### code to check reverese of a word 
 
+# string = input("Enter a string: ")
+
+# words_split = string.split()
+
+# reversed_words = ""
+
+# for word in words_split:
+#     reversed_word = word[::-1]
+#     reversed_words += reversed_word + " "
+
+# reversed_words = reversed_words.strip()
+
+# print(reversed_words)
+
+
+
 string = input("Enter a string: ")
 
 words_split = string.split()
 
 reversed_words = ""
 
-for word in words_split:
-    reversed_word = word[::-1]
-    reversed_words += reversed_word + " "
+for word in reversed(words_split):
+    reversed_words += word + " "
 
 reversed_words = reversed_words.strip()
 
