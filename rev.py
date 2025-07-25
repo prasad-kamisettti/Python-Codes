3db5e27102cab8bb434b4121707b90b953bb0ef0
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-07-25 08:37:05 -0500

    added comments to the code

--- rev.py
+++ rev.py
@@ -1,25 +1,26 @@
 
 ### code to check reverese of a word 
 
-# string = input("Enter a string: ")
-
-# words_split = string.split()
-
-# reversed_words = ""
-
-# for word in words_split:
-#     reversed_word = word[::-1]
-#     reversed_words += reversed_word + " "
-
-# reversed_words = reversed_words.strip()
-
-# print(reversed_words)
-
-
-
 string = input("Enter a string: ")
 
 words_split = string.split()
+
+reversed_words = ""
+
+for word in words_split:
+    reversed_word = word[::-1]
+    reversed_words += reversed_word + " "
+
+reversed_words = reversed_words.strip()
+
+print(reversed_words)
+
+
+### code to reverse a whole string strig 
+string = input("Enter a string: ")
+
+# split input string 
+words_split = string.split()    
 
 reversed_words = ""
 
