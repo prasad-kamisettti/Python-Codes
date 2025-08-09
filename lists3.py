0d721c20a09c9546b38b6be87e7e70960319f22c
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-08-09 13:05:38 -0500

    psuhing another excercise on lists

---
+++ lists3.py
@@ -0,0 +1,41 @@
+# Let us say your expense for every month are listed below,
+# January - 2200
+# February - 2350
+# March - 2600
+# April - 2130
+# May - 2190
+# Create a list to store these monthly expenses and using that find out,
+
+
+# 1. In Feb, how many dollars you spent extra compare to January?
+# 2. Find out your total expense in first quarter (first three months) of the year.
+# 3. Find out if you spent exactly 2000 dollars in any month
+# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+# 5. You returned an item that you bought in a month of April and
+# got a refund of 200$. Make a correction to your monthly expense list
+# based on this
+
+expense = ["January 2200", "feb 2350", "march 2600", "april 2130", "may 2190"]
+print(expense)
+
+
+print(int(expense[1].split()[1]) - int(expense[0].split()[1]))
+
+print(int(expense[0].split()[1]) + int(expense[1].split()[1]) + int(expense[3].split()[1])) 
+
+print(2000 in [int(item.split()[1]) for item in expense])
+
+expense.insert(5, "June: 1980") 
+print(expense)
+
+expense = ["January 2200", "feb 2350", "march 2600", "april 2130", "may 2190"]
+
+
+
+
+
+
+
+
+
+
