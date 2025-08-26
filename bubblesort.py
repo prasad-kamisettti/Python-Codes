d541f95d2d2772b4ce5d185708ed932d6d458915
prasad-kamisettti <prasadkamisetti96@gmail.com>
2025-08-25 19:36:07 -0500

    added comments

--- bubblesort.py
+++ bubblesort.py
@@ -1,10 +1,10 @@
-li = list(map(int, input("Enter numbers separated by space: ").split()))
+li = list(map(int, input("Enter numbers separated by space: ").split())) #user inputs
 
 
 n = len(li)
 
 for i in range (n-1):
     for j in range(n-1-i): # inner loop for comparison
-        if li[j]>li[j+1]:
-            li[j],li[j+1] = li[j+1],li[j]
+        if li[j]>li[j+1]: 
+            li[j],li[j+1] = li[j+1],li[j] #Swapping
 print(li)  
